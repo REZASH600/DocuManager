adf635f04fa8d81d801d9f592d9bbaa64ce4e60d
REZASH600 <rezasharafdini973@gmail.com>
2025-08-06 18:33:56 +0330

    feat(signals): add signal to enforce single active Document per DocumentType

--- core/apps/documents/signals.py
+++ core/apps/documents/signals.py
@@ -30,3 +30,28 @@ def soft_delete_related_types_and_documents_and_text_files(sender, instance, **k
         UploadedTextFile.objects.filter(document_type__category=instance).delete()
 
 
+
+
+
+
+@receiver(pre_save, sender=Document)
+def ensure_single_active_document_per_type(sender, instance, **kwargs):
+    """
+    Ensure only one active Document per DocumentType.
+    """
+    if instance.is_deleted:
+        return  # Ignore deleted documents
+
+    if instance.is_active:
+        Document.objects.filter(
+            document_type=instance.document_type,
+            is_active=True,
+        ).exclude(pk=instance.pk).update(is_active=False)
+    else:
+        has_active = Document.objects.filter(
+            document_type=instance.document_type,
+            is_active=True
+        ).exclude(pk=instance.pk).exists()
+
+        if not has_active:
+            instance.is_active = True
